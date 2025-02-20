2d0309fb720fdf00853fcaab8dcf4b4b364d1787
Makhmudova Munira <105048261+makhmudovamunira@users.noreply.github.com>
2025-02-20 13:25:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ from fastai.vision.all import *
 
 #title
 st.title('Transportlarni klassifikatsiya qiluvchi model')
+
+file=st.file_uploader('rasm yuklash', type=['png', 'jpeg', 'gif', 'svg']
+
+if file:
+  st.image(file)
+  #PIL convert
+                      
