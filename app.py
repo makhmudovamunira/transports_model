ece00d62443ff9cff253aabe38df7f00057ab903
makhmudovamunira <105048261+makhmudovamunira@users.noreply.github.com>
2025-02-20 13:48:20 +0500

    model tayyor

--- app.py
+++ app.py
@@ -9,4 +9,11 @@ file=st.file_uploader('rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
 if file:
   st.image(file)
   #PIL convert
-                      
+  img=PILImage.create(file)
+  #model
+  model=load_learner('transport_model.pkl')
+
+  #prediction
+  pred, pred_id, probs=model.predict(img)
+  st.success(f"bashorat {pred}")
+  st.info(f"Ehtimollik {probs[pred_id]*100:.2f}%")                   
